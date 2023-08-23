5a4461458fc47889c23743907c7e7f7d66730cbf
amaanlari <amaanlari@outlook.com>
2023-08-23 10:38:58 +0530

    Added Image composable of taskCompletedImage image

--- app/src/main/java/com/lari/taskmanager/MainActivity.kt
+++ app/src/main/java/com/lari/taskmanager/MainActivity.kt
@@ -3,12 +3,51 @@ package com.lari.taskmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            TaskCompleteScreen()
         }
     }
+}
+
+@Composable
+fun TaskCompleteScreen() {
+    val taskCompletedImage = painterResource(R.drawable.ic_task_completed)
+
+    Column(modifier = Modifier
+        .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = taskCompletedImage,
+            contentDescription = null,
+            alignment = Alignment.Center,
+            contentScale = ContentScale.Fit
+        )
+    }
+}
+
+@Preview
+@Composable
+fun TaskManagerPreview() {
+        TaskCompleteScreen()
 }
